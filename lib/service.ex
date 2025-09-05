408b6afe5090ab213bced51a15fe364584db04f0
José Arthur de Mello Mulatinho <62631403+jamm2k@users.noreply.github.com>
2025-09-05 12:25:46 -0300

    queria star morta (#10)
    Co-authored-by: zoey <zoey.spessanha@zeetech.io>

--- lib/service.ex
+++ lib/service.ex
@@ -9,6 +9,7 @@ defmodule Service do
 
   @participants [
     {"Zoey", "zoedsoupe"},
+    {"José Arthur", "jamm2k"},
     {"Fred", "Sader"},
     {"Juan", "juanzeen"},
     {"Mateus", "mmcferreira"}
